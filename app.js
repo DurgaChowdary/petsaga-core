f5b6a1dc8a9105c1f23376898b8815714576d7f2
E Durga Chowdary <honeyeadala16@gmail.com>
2025-07-03 12:00:48 -0400

    Adding Model files for all the DB collections

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 2300;
 
 app.use(express.json()); // Middleware for parsing JSON
 
+// Setting up a Mongoose object for establishing a DB connection
+mongoose.connect('mongodb://localhost:27017/petsaga-dev', {
+     useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connection established'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 // App Routes
 const PetRouter = require('./routes/pets');
